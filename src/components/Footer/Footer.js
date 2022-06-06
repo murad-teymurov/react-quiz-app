e057b0db99cc932a31d5bda537554dcf25456ba1
Murad <muradteymurov2000@gmail.com>
2022-06-06 09:33:38 +0400

    loaded

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      
+      <a
+        href="https://learn.algoritmika.org/login"
+        style={{ cursor: "pointer" }}
+      >
+       Made with ♥ by WEB-6
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
